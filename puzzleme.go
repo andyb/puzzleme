264d3c7017bc86f80d7022dc2033fffdb198d979
Andy B <me@andybritcliffe.com>
2013-03-06 08:55:18 +0000

    reworked the function calls and added a struct to represent the image slice

--- puzzleme.go
+++ puzzleme.go
@@ -5,15 +5,24 @@ import "image/jpeg"
 import "image/draw"
 import "os"
 import "log"
-import "time"
+import "strconv"
 
-func main() {
-	LoadImage()
+type ImageSlice struct {
+	img         image.Image
+	sliceNumber int
+	totalSlices int
+	fileName    string
 }
 
-func LoadImage() {
+func main() {
+	img := LoadImage("testdata/img-1.jpg")
+	SplitImagesAndSave(img)
+}
+
+/*load the image to be sliced*/
+func LoadImage(fileName string) image.Image {
 	// Open the file.
-	file, err := os.Open("testdata/img-1.jpg")
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,34 +33,34 @@ func LoadImage() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return img
+}
 
+/* Split the image into the required number of slices and save out */
+func SplitImagesAndSave(img image.Image) {
 	chans := map[int]chan int{}
 	totalSlices := 4
 	for i := 1; i <= totalSlices; i++ {
-		//need to work out how to block whie these complete? Buffered channel??
 		chans[i] = make(chan int)
-		go ChopImage(img, i, totalSlices, chans[i])
+		go chopImage(ImageSlice{img, i, totalSlices, "temp"}, chans[i])
 	}
 
 	//loop through the channels and block until channel for each returns
 	for i := 1; i <= totalSlices; i++ {
 		<-chans[i]
 	}
-
-	WriteImageToFile()
 }
 
-func ChopImage(img image.Image, sliceNumber int, totalSlices int, completed chan int) {
+/* Responsible for taking the image and the current slice and create a slice image. Will notify when complete via the channel */
+func chopImage(slice ImageSlice, completed chan int) {
 	//need to divide the image up by the number of slices. 
+	rect := slice.img.Bounds()
+	size := rect.Size()
 
-	sliceImg := image.NewRGBA(image.Rect(0, 0, 400, 400))
-	draw.Draw(sliceImg, sliceImg.Bounds(), img, image.ZP, draw.Src)
-	toSave, _ := os.Create("out/temp.jpg")
+	sliceImg := image.NewRGBA(image.Rect(0, 0, size.X/2, size.Y/2))
+	draw.Draw(sliceImg, sliceImg.Bounds(), slice.img, image.ZP, draw.Src)
+	toSave, _ := os.Create("out/" + slice.fileName + strconv.Itoa(slice.sliceNumber) + ".jpg")
 	jpeg.Encode(toSave, sliceImg, nil)
-	log.Printf("Chop Chop %v", sliceNumber)
-	completed <- sliceNumber
-}
-
-func WriteImageToFile() {
-	log.Println("Writing image to file")
+	log.Printf("Chop Chop %v", slice.sliceNumber)
+	completed <- slice.sliceNumber
 }
