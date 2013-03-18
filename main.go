974c309fa9c2271bc379383926de37ee2dbdad8f
Andy Britcliffe <abritcliffe@sportingindex.com>
2013-03-18 16:57:15 +0000

    added ability to server images

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "./imageslice"
-import "./server"
+import (
+	"github.com/andyb/web"
+	"log"
+)
+
+func index() string { return "hello" }
+
+func image(ctx *web.Context, val string) {
+	ctx.WriteImage("out/" + val + ".jpg")
+	log.Println(val)
+}
 
 func main() {
-	img := imageslice.LoadImage("testdata/img-1.jpg")
-	imageslice.SplitImagesAndSave(img)
-	server.Start()
+	web.Get("/images/(.*)", image)
+	web.Get("/", index)
+	web.Run("0.0.0.0:8080")
 }
