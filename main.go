0e7fd0c5d8898ab72259b3e05679138e9087d7db
Andy Britcliffe <abritcliffe@sportingindex.com>
2013-03-21 07:49:40 +0000

    added bootstrap and a simple index.html

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	//"./imageslice"
 	"github.com/andyb/web"
 	"log"
 )
 
-func index() string { return "hello" }
+func index(ctx *web.Context, val string) {
+	ctx.Redirect(301, "/index.html")
+}
+
+func slice(ctx *web.Context, val string) {
+	ctx.WriteString("ok")
+}
 
 func image(ctx *web.Context, val string) {
 	ctx.WriteImage("out/" + val + ".jpg")
@@ -14,6 +21,7 @@ func image(ctx *web.Context, val string) {
 
 func main() {
 	web.Get("/images/(.*)", image)
-	web.Get("/", index)
+	//web.Get("/", index)
+	web.Post("/slice/(.*)", slice)
 	web.Run("0.0.0.0:8080")
 }
