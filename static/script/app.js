af001a2ee04bc326d9d936c632093c7d0355d49b
Andy B <me@andybritcliffe.com>
2013-04-10 08:00:41 +0100

    added boilerplate code for showing a sneak peek of the solution

--- static/script/app.js
+++ static/script/app.js
@@ -12,3 +12,28 @@ $(function() {
     });
   
 });
+
+
+function main($scope) {    
+     $scope.puzzleSlices = [
+        {img:'temp7'},
+        {img:'temp1'},
+        {img:'temp8'},
+        {img:'temp3'},
+        {img:'temp9'},
+        {img:'temp5'},
+        {img:'temp2'},
+        {img:'temp4'},
+        {img:'temp6'}
+     ]
+
+     $scope.peek = function() {
+         $("#puzzle").hide()
+         $("#peek").show()
+     }
+
+     $scope.puzzle = function() {
+         $("#puzzle").show()
+         $("#peek").hide()
+     }
+}
